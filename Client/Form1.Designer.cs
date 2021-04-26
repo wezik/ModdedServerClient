4cbadd7e81b38d89e613fc42f578bacdf24a0ff8
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-04-26 23:12:35 +0200

    Initial commit

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Instaluj Mody";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 53);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(140, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ściągnij Forge\'a";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(164, 53);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(134, 23);
+            this.progressBar2.TabIndex = 3;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(9, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Instalacja modów nadpisze wszystkie mody w folderze mods";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(309, 115);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "MangoEZ Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private static String path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private String dir = path + "/.minecraft/mods";
+        private String fileName = "/temp.zip";
+        private static bool extracted = false;
+        private static bool forgeDownloaded = false;
+        private String currDir = Directory.GetCurrentDirectory();
+
+        //Link to direct download forge version
+        private String forgeUrl = "";
+
+        //Link to direct download for your Modpack 
+        private String modsWinrarUrl = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            downloadModFiles();
+        }
+
+        private void extractFiles()
+        {
+            ZipFile.ExtractToDirectory(dir + fileName,dir);
+            File.Delete(dir + fileName);
+            button2.Text = "Mody Zainstalowane";
+        }
+
+        private void downloadModFiles()
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            String[] files = Directory.GetFiles(dir);
+            foreach (String file in files)
+            {
+                File.Delete(file);
+            }
+            using (WebClient wc = new WebClient())
+            {
+                String url = wc.DownloadString(modsWinrarUrl);
+                wc.DownloadProgressChanged += wc_DownloadModProgressChanged;
+                wc.DownloadFileAsync(
+                    new System.Uri(url),
+                    dir + fileName);
+            }
+        }
+
+        void wc_DownloadModProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            if (e.ProgressPercentage == 100 && !extracted)
+            {
+                extracted = true;
+                extractFiles();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            downloadForge();
+        }
+
+        private void downloadForge()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                String url = wc.DownloadString(forgeUrl);
+                wc.DownloadProgressChanged += wc_DownloadForgeProgressChanged;
+                wc.DownloadFileAsync(
+                    new System.Uri(url),
+                    currDir + "/forge.jar");
+            }
+        }
+
+        void wc_DownloadForgeProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar2.Value = e.ProgressPercentage;
+            if (e.ProgressPercentage == 100 && !forgeDownloaded)
+            {
+                forgeDownloaded = true;
+                button3.Text = "Forge ściągnięty";
+                MessageBox.Show("Forge znajduje się w\n"+currDir);
+            }
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
